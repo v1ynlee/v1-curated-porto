f2d3de91aee1ea1cce61ed2eca1088942cc0e45f
v1ynlee <chikaamatori27@gmail.com>
2026-06-09 03:04:35 +0700

    feat: add lenis provider

---
+++ src/components/providers/lenis-provider.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
+
+export function LenisProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.1,
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      touchMultiplier: 2,
+      infinite: false,
+    });
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    const rafId = requestAnimationFrame(raf);
+
+    // Make lenis available globally for anchor scrolling
+    (window as typeof window & { lenis?: Lenis }).lenis = lenis;
+
+    return () => {
+      cancelAnimationFrame(rafId);
+      lenis.destroy();
+    };
+  }, []);
+
+  return <>{children}</>;
+}

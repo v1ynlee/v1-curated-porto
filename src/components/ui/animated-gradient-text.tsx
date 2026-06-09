535839e318499a8832dd851d46a9aa999c2d7f8a
v1ynlee <chikaamatori27@gmail.com>
2026-06-09 21:40:49 +0700

    style: format animated gradient text

--- src/components/ui/animated-gradient-text.tsx
+++ src/components/ui/animated-gradient-text.tsx
@@ -1,11 +1,11 @@
-import { type ComponentPropsWithoutRef } from "react"
+import { type ComponentPropsWithoutRef } from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 export interface AnimatedGradientTextProps extends ComponentPropsWithoutRef<"div"> {
-  speed?: number
-  colorFrom?: string
-  colorTo?: string
+  speed?: number;
+  colorFrom?: string;
+  colorTo?: string;
 }
 
 export function AnimatedGradientText({
@@ -27,11 +27,11 @@ export function AnimatedGradientText({
       }
       className={cn(
         `animate-gradient inline bg-linear-to-r from-(--color-from) via-(--color-to) to-(--color-from) bg-size-[var(--bg-size)_100%] bg-clip-text text-transparent`,
-        className
+        className,
       )}
       {...props}
     >
       {children}
     </span>
-  )
+  );
 }
